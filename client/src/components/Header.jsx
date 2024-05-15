5834b2a4ff8cbc05dcd95a8eb0ed2ba693f39ecb
Carlos-860 <carlostimotheus5@gmail.com>
2024-05-15 18:19:54 +0200

    sign up page ui & header style changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,30 +4,32 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
-		<header className='bg-slate-200 shadow-md'>
-			<div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-				<Link to="/">
-					<h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-						<span className='text-slate-500'>Sahand</span>
-						<span className='text-slate-700'>Estate</span>
-					</h1>
-				</Link>
-				<form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-					<input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-					<FaSearch className='text-slate-600' />
-				</form>
-				<ul className='flex gap-4'>
+		<header>
+			<nav className='bg-white shadow'>
+				<div className='flex justify-between items-center mx-auto max-w-7xl p-3'>
 					<Link to="/">
-						<li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+						<h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+							<span className='text-slate-500'>Sahand</span>
+							<span className='text-slate-700'>Estate</span>
+						</h1>
 					</Link>
-					<Link to="/about">
-						<li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-					</Link>
-					<Link to="/sign-in">
-						<li className='text-slate-700 hover:underline'>Sign in</li>
-					</Link>
-				</ul>
-			</div>
+					<form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+						<input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+						<FaSearch className='text-slate-600' />
+					</form>
+					<ul className='flex gap-4'>
+						<Link to="/">
+							<li className='hidden sm:inline text-slate-700 hover:underline text-sm font-medium'>Home</li>
+						</Link>
+						<Link to="/about">
+							<li className='hidden sm:inline text-slate-700 hover:underline text-sm font-medium'>About</li>
+						</Link>
+						<Link to="/sign-in">
+							<li className='inline text-slate-700 hover:underline text-sm font-medium'>Sign in</li>
+						</Link>
+					</ul>
+				</div>
+			</nav>
 		</header>
 	)
 }
