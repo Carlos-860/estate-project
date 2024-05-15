55deafe1f6f2bd9a7a588cf46c0ef19d338d6872
Carlos-860 <carlostimotheus5@gmail.com>
2024-05-15 18:01:54 +0200

    Create a middleware and error handling function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,16 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); // hashSync allows us to not use await since it already incoporates it.
     const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save(); // code remains in this line before executing next line
         res.status(201).json("User created successfully");
+
     } catch (error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
